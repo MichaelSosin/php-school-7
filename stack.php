52bd9b47dfaf3d635bb0eb808b845f9cfdf5fae6
MichaelSosin <mihail.ms93@gmail.com>
2018-12-24 15:04:14 +0200

    add stack class

---
+++ stack.php
@@ -0,0 +1,57 @@
+<?php
+
+interface iStack {
+    public function push($item);
+    public function pop();
+    public function top();
+    public function isEmpty();
+}
+
+class Stack implements iStack {
+    private $_data = [];
+    private $_length = 5;
+    private $_topIndex = -1; 
+    
+    public function __construct($length = NULL) {
+        $this->_setLength($length);
+    }
+
+    public function push($item) {
+        if ($this->_topIndex >= $this->_length) {
+            throw new Exception('Stack overflow');
+        }
+
+        $this->_data[] = $item;
+        $this->_topIndex++;
+    }
+
+    public function pop() {
+        if ($this->isEmpty()) {
+            throw new Exception('Stack underflow');
+        }
+
+        $itemToPop = $this->_data[$this->_topIndex];
+        unset($this->_data[$this->_topIndex]);
+
+        $this->_topIndex--;
+        return $itemToPop;
+    }
+
+    public function top() {
+        if ($this->isEmpty()) {
+            throw new Exception('Stack is empty');
+        }
+
+        return $this->_data[$this->_topIndex];
+    }
+
+    public function isEmpty() {
+        return $this->_topIndex <= -1;
+    }
+
+    private function _setLength($length) {
+        if ($length !== NULL) {
+            $this->_length = $length;
+        }
+    }
+}
