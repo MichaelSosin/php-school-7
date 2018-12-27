fedeff969d90b8d64346e7f8ab7ccf0266823c86
MichaelSosin <mihail.ms93@gmail.com>
2018-12-27 14:59:34 +0200

    add queue implemented with two stacks

---
+++ squeue.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once 'types.php';
+include_once 'stack.php';
+
+class SQueue implements iQueue {
+    private $inbox = NULL;
+    private $outbox = NULL;
+    private $length = 5;
+
+    public function __construct($length = NULL) {
+        $this->_setLength($length);
+
+        $this->inbox = new Stack($this->length);
+        $this->outbox = new Stack($this->length);
+    }
+
+    public function enqueue($item) {
+        try {
+            $this->inbox->push($item);
+        }
+        catch (Exception $e) {
+            throw new Exception('Queue overflow');
+        }
+    }
+
+    public function dequeue() {
+        if ($this->outbox->isEmpty()) {
+            while (!$this->inbox->isEmpty()) {
+                $this->outbox->push($this->inbox->pop());
+            }
+        }
+
+        try {
+            $itemToDequeue = $this->outbox->pop();
+        }
+        catch (Exception $e) {
+            throw new Exception('Queue underflow');
+        }
+
+        return $itemToDequeue;
+    }
+
+    public function isEmpty() {
+        
+    }
+
+    private function _setLength($length) {
+        if ($length !== NULL) {
+            $this->length = $length;
+        }
+    }
+}
